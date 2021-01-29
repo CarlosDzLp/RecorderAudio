251f281ca216199a6cc7c433c0ccd94886c9df38
Carlos Diaz <ryankar90@hotmail.com>
2021-01-28 21:38:11 -0600

    create project

---
+++ RecorderAudioXam/RecorderAudioXam.iOS/AppDelegate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AVFoundation;
+using Foundation;
+using Plugin.AudioRecorder;
+using UIKit;
+
+namespace RecorderAudioXam.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            AudioRecorderService.RequestAVAudioSessionCategory(AVAudioSessionCategory.PlayAndRecord);
+            AudioPlayer.RequestAVAudioSessionCategory(AVAudioSessionCategory.PlayAndRecord);
+            AudioPlayer.OnPrepareAudioSession = audioSession =>
+            {
+                var success = audioSession.OverrideOutputAudioPort(AVAudioSessionPortOverride.Speaker, out NSError error);
+            };
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
---
+++ RecorderAudioXam/RecorderAudioXam/MainPage.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.AudioRecorder;
+using Xamarin.Forms;
+
+namespace RecorderAudioXam
+{
+    public partial class MainPage : ContentPage
+    {
+        AudioRecorderService recorder;
+        AudioPlayer player;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            recorder = new AudioRecorderService
+            {
+                StopRecordingAfterTimeout = true,
+                TotalAudioTimeout = TimeSpan.FromSeconds(10),
+                AudioSilenceTimeout = TimeSpan.FromSeconds(2)
+            };
+            player = new AudioPlayer();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                RecordAudio();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        void RecordAudio()
+        {
+            try
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    if (!recorder.IsRecording)
+                    {
+                        recorder.StopRecordingOnSilence = false;
+                        //start recording audio
+                        var audioRecordTask = await recorder.StartRecording();
+                        await audioRecordTask;
+
+                        getAudio();
+                    }
+                    else //Stop button clicked
+                    {
+                        //await recorder.StopRecording();
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                //blow up the app!
+                throw ex;
+            }
+        }
+
+        private void getAudio()
+        {
+            try
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    //var audio = recorder.GetAudioFilePath();
+
+                    //OBTIENE EL STREAM PARA PODER MANIPULARLO Y CONVERTILO A BYTES
+                    var filePath = recorder.GetAudioFileStream();
+
+                    if (filePath != null)//VALIDAS QUE NO ESTE NULL EL STREAM
+                    {
+                        
+                        var bytes = ReadStream(filePath);//CONVIERTES EL STREAM A BYTE PARA ENVIARLO A UN WEB SERVICE
+
+
+                        var convertBase64 = Convert.ToBase64String(bytes);//POR SI SE QUIERE CONVERTIR A BASE64
+
+                        player.Play(recorder.FilePath);//SE OBTIENE EL PATH DEL ARCHIVO DE AUDIO
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                //blow up the app!
+
+            }
+        }
+
+        public static byte[] ReadStream(Stream input)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
